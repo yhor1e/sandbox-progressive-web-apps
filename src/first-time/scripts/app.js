fa04bf6e8bdf76705e01cd3c01c0160421b6da88
yhorie <yuk.horie@gmail.com>
2016-08-04 00:11:54 +0900

    Complete 初回実行時と区別する

--- src/first-time/scripts/app.js
+++ src/first-time/scripts/app.js
@@ -69,6 +69,7 @@
     app.selectedCities.push({key: key, label: label});
     // Remember to save user preferences here
     app.toggleAddDialog(false);
+    app.saveSelectedCities();
   });
 
   document.getElementById('butAddCancel').addEventListener('click', function() {
@@ -186,5 +187,38 @@
 
   // Add code to check if the user has any subscribed cities, and render
   // those or the default data here.
+
+  // Save list of cities to localStorage, see note below about localStorage.
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    // IMPORTANT: See notes about use of localStorage.
+    localStorage.selectedCities = selectedCities;
+  };
+
+  /****************************************************************************
+   *
+   * Code required to start the app
+   *
+   * NOTE: To simplify this getting started guide, we've used localStorage.
+   *   localStorage is a synchronous API and has serious performance
+   *   implications. It should not be used in production applications!
+   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+   *
+   ****************************************************************************/
+
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
 
 })();
