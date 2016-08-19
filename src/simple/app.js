254d1962b1dcfb1d6327e7ef7b370e5aedca0e9f
yhorie <yuk.horie@gmail.com>
2016-08-19 22:56:11 +0900

    Polish

--- src/simple/app.js
+++ src/simple/app.js
@@ -1,11 +1,7 @@
-var hasRequestPending=false;
-
 window.addEventListener('DOMContentLoaded', function(){
   window.fetch('./data.json').then(function(response) {
-    hasRequestPending=true;
     response.json().then(
       function(json){
-        hasRequestPending=false;
         console.log('fetched data is `', json.data, '`')
         var bodyEls =document.getElementsByTagName('body');
         bodyEls[0].textContent = json.data;
@@ -16,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
-    console.info('ServiceWorker registration successful with scope: ',    registration.scope);
+    console.info('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(function(err) {
     console.error('ServiceWorker registration failed: ', err);
   });
